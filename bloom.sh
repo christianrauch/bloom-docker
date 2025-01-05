c46c89608a84fffd4e7a886781630024118ac6aa
Christian Rauch <Rauch.Christian@gmx.de>
2025-01-06 00:29:08 +0100

    manually patch "compat" to enable paralle builds with

--- bloom.sh
+++ bloom.sh
@@ -6,6 +6,8 @@ apt update
 
 pushd /bloom/package
 bloom-generate rosdebian --ros-distro jazzy
+# bump compat to 10 to enable parallel builds
+echo "10" > debian/compat
 mk-build-deps
 apt install -y --no-install-recommends ./ros-jazzy-*-build-deps_*_all.deb
 dpkg-buildpackage -b
